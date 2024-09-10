8c51dd21dd2aff9c6be23629cd68ab88fd518694
labf402 <7707763@continental.edu.pe>
2024-09-10 14:24:26 -0500

    Actualizado

--- Actividad1.py
+++ Actividad1.py
@@ -1,3 +1,25 @@
-class X:
-  def z(self, x, y):
-    return x * y if y != 0 else x / y
+class Calculadora:
+    def sumar(self, Termino1, Termino2):
+        return Termino1 + Termino2
+
+    def restar(self, Termino1, Termino2):
+        return Termino1 - Termino2
+
+
+def ProbarSuma(Termino1, Termino2):
+    calc = Calculadora()
+    CalcularSuma = calc.sumar(Termino1, Termino2)
+    print(f"{Termino1} + {Termino2} = {CalcularSuma}")
+
+
+def ProbarResta(Termino1, Termino2):
+    calc = Calculadora()
+    CalcularResta = calc.restar(Termino1, Termino2)
+    print(f"{Termino1} - {Termino2} = {CalcularResta}")
+
+
+if __name__ == "__main__":
+    variable1 = float(input("Ingrese el primer Termino: "))
+    variable2 = float(input("Ingrese el segundo Termino: "))
+    ProbarSuma(variable1, variable2)
+    ProbarResta(variable1, variable2)
